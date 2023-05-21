2cb98aa70d8111b7eb77b863150df5811c5d34d9
Luiz Anacleto <61395722+lanacleto@users.noreply.github.com>
2023-05-21 20:35:39 -0300

    Delete api directory

--- api/src/config/jwt.js
+++
@@ -1,4 +0,0 @@
-export default {
-    secret: '93df908543ca4360c273ff87fc49bd37',
-    expiresIn: '5d'
-}
--- api/src/server.js
+++
@@ -1,6 +0,0 @@
-import app from './app'
-require('dotenv').config()
-
-app.listen(process.env.PORT, () => {
-    console.log(`http://localhost:${process.env.PORT}`)
-})
